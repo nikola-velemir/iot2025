9ddc8da32c3a6e0247c8730ec5970aa2ca1d92ec
nikola-velemir <vnexa021@gmail.com>
2025-12-22 23:55:21 +0100

    [feature] both PI1 simulation

---
+++ PI1/main.py
@@ -0,0 +1,38 @@
+import threading
+import time
+
+from shared.config import load_config
+from shared.logger.logger import log, logger_loop
+from shared.sensors.door_motion_sensor.component import run_motion_sensor
+from shared.sensors.door_sensor.component import run_door_sensor
+from shared.sensors.door_ultra_sonic.component import run_ultrasonic_sensor
+from shared.sensors.keypad.component import run_keypad
+
+if __name__ == '__main__':
+    config = load_config("PI1/config.json")
+    print(config)
+    threads = []
+    stop_event = threading.Event()
+    try:
+        ds1_settings = config['DS1']
+        run_door_sensor(ds1_settings, threads, stop_event)
+        dus1_settings = config['DUS1']
+        run_ultrasonic_sensor(dus1_settings, threads, stop_event)
+
+        dpir1_settings = config['DPIR1']
+        run_motion_sensor(dpir1_settings, threads, stop_event)
+        dms_settings = config['DMS']
+        run_keypad(dms_settings, threads,stop_event)
+        threading.Thread(
+            target=logger_loop,
+            args=(stop_event,),
+            daemon=True
+        ).start()
+        while True:
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        log('Stopping app')
+        for t in threads:
+            stop_event.set()
+
