883502e0bc28636e40c2136d705403fa6e061bc0
nikola-velemir <vnexa021@gmail.com>
2026-02-11 19:30:23 +0100

    [feature] pin validator

---
+++ shared/pin_validator.py
@@ -0,0 +1,19 @@
+
+def validated(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+
+        presence_set = set()
+        for v in result.values():
+            pins = v.get('pins',{}).values()
+            is_simulated = v.get('simulated',True)
+            if not is_simulated and len(pins) == 0:
+                raise ValueError("You have not specified some pins")
+
+            for pin in pins:
+                if pin in presence_set:
+                    raise ValueError("Some pins repeat in your config")
+                presence_set.add(pin)
+
+        return result
+    return wrapper
