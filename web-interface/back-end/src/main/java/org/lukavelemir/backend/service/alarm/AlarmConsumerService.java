e59e7cb221cfeb72e75ad165193f3b8eb246ad83
Luka <79410671+lukascobbler@users.noreply.github.com>
2026-02-14 23:00:45 +0100

    feature: alarm consumer service implementation

--- web-interface/back-end/src/main/java/org/lukavelemir/backend/service/alarm/AlarmConsumerService.java
+++ web-interface/back-end/src/main/java/org/lukavelemir/backend/service/alarm/AlarmConsumerService.java
@@ -2,14 +2,14 @@ package org.lukavelemir.backend.service.alarm;
 
 import lombok.RequiredArgsConstructor;
 import org.lukavelemir.backend.service.common.GlobalState;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.simp.user.SimpUser;
 import org.springframework.stereotype.Service;
 import tools.jackson.databind.JsonNode;
 import tools.jackson.databind.ObjectMapper;
 
-/// Updates the state of the alarm.
+import java.util.concurrent.locks.ReentrantLock;
 
 @RequiredArgsConstructor
 @Service
@@ -17,6 +17,10 @@ public class AlarmConsumerService {
     private final GlobalState globalState;
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final AlarmProducerService alarmProducerService;
+    private final ReentrantLock armingLock = new ReentrantLock();
+
+    @Value("${app.alarm.pin}")
+    private String alarmPin;
 
     @ServiceActivator(inputChannel = "alarmConsumerChannel")
     public void consume(Message<String> message) {
@@ -28,8 +32,8 @@ public class AlarmConsumerService {
             switch (type) {
                 case "try_arm" -> armAlarm(json.path("pin").stringValue());
                 case "person_event" -> updatePeopleInBuilding(json.get("person_event").stringValue());
-                case "motion" -> motionDetected("motion sensor");
-                case "gyro" -> motionDetected("gyro");
+                case "motion" -> motionDetectedPir();
+                case "gyro" -> motionDetectedGyro();
                 case "open_too_long" -> doorButtonNotPressedForMoreThan5Seconds();
                 default -> System.out.println("Unknown message type received: " + type);
             }
@@ -41,26 +45,66 @@ public class AlarmConsumerService {
 
     private void updatePeopleInBuilding(String event) {
         switch (event) {
-            case "left" -> System.out.println("PERSON LEFT");
-            case "entered" -> System.out.println("PERSON ENTERED");
+            case "left" -> globalState.decrementPersonCount();
+            case "entered" -> globalState.incrementPersonCount();
             default -> throw new RuntimeException("Incorrect type");
         }
     }
 
     private void armAlarm(String pin) {
-        System.out.println("ALARM TRIED TO BE ARMED WITH PIN: " + pin);
+        if (!armingLock.tryLock()) {
+            System.out.println("REQUEST REJECTED: Arming process already in progress.");
+            return;
+        }
+
+        try {
+            if (pin.equals(alarmPin)) {
+                if (globalState.isAlarmArmed()) {
+                    System.out.println("ALARM DISARMED: " + pin);
+                    globalState.turnOffAlarm();
+                    globalState.disarmAlarm();
+                    alarmProducerService.sendAlarmDeactivationSignal();
+                } else {
+                    System.out.println("STARTING 10s ARM COUNTDOWN");
+                    Thread.sleep(10000);
+                    globalState.armAlarm();
+                    System.out.println("ALARM ARMED: " + pin);
+                }
+            } else {
+                globalState.turnOnAlarm();
+                globalState.armAlarm();
+                alarmProducerService.sendAlarmActivationSignal();
+                System.out.println("PIN NOT CORRECT, TURNING ON ALARM: " + pin);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            armingLock.unlock();
+        }
     }
 
-    private void motionDetected(String type) {
-        System.out.println("MOTION DETECTED BY " + type);
+    private void motionDetectedPir() {
+        System.out.println("MOTION DETECTED BY PIR");
+
+        if (globalState.getPersonCount() == 0) {
+            activateAlarmIfArmed();
+        }
+    }
+
+    private void motionDetectedGyro() {
+        System.out.println("MOTION DETECTED BY GYRO");
+        activateAlarmIfArmed();
     }
 
     private void doorButtonNotPressedForMoreThan5Seconds() {
         System.out.println("DOOR OPENED FOR MOE THAN 5 SECONDS");
-        alarmProducerService.sendAlarmActivationSignal();
-
+        activateAlarmIfArmed();
     }
 
     private void activateAlarmIfArmed() {
+        if (globalState.isAlarmArmed()) {
+            globalState.turnOnAlarm();
+            alarmProducerService.sendAlarmActivationSignal();
+        }
     }
 }
