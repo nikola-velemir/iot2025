6e631ee616dbd8970eefe16afe2ca752cbd514c9
nikola-velemir <vnexa021@gmail.com>
2026-02-14 22:08:03 +0100

    [fix] brgb

--- peripherals/shared/actuators/brgb/actuator.py
+++ peripherals/shared/actuators/brgb/actuator.py
@@ -10,11 +10,13 @@ class BRGB(Subscriber):
     def __init__(self, output, name, device_name, mqtt_client):
         self.output = output
         self.name = name
-        self.telegraf_client = mqtt_client
+        self._telegraf_client = mqtt_client
         self.device_name = device_name
-        self.current_color = "OFF"
-        self.receive_client = MqttReceiver("brgb", self.msg_callback)
-        self.receive_client.start()
+
+        self._is_on = False
+        self._current_color = "WHITE"
+        self._receive_client = MqttReceiver("brgb", self.msg_callback)
+        self._receive_client.start()
 
     def msg_callback(self, topic, payload):
         if "BRGB_NEW_LIGHT" in payload:
@@ -25,22 +27,28 @@ class BRGB(Subscriber):
         log(f"[{self.name}] Actuator processing command: {event}")
 
         if event == "ON":
+
             self.turn_on()
+            self.set_color("WHITE")
         elif event == "OFF":
             self.turn_off()
         else:
+            if not self._is_on:
+                self.turn_on()
             self.set_color(event)
     def callback(self, event):
        self._handle_color_change(event)
 
     def set_color(self, color):
-        self.current_color = color
+        self._current_color = color
         log(f"{self.name} changed color to {color}")
 
     def turn_on(self):
-        self.current_color = "WHITE"
+        if self._is_on:
+            return
         log(f"{self.name} turned ON")
-        self.telegraf_client.send(
+        self._is_on = True
+        self._telegraf_client.send(
             MqttTelegrafPoint(
                 "BRGB",
                 self.device_name,
@@ -51,9 +59,11 @@ class BRGB(Subscriber):
         )
 
     def turn_off(self):
-        self.current_color = "OFF"
+        if not self._is_on:
+            return
         log(f"🌑 {self.name} turned OFF")
-        self.telegraf_client.send(
+        self._is_on = False
+        self._telegraf_client.send(
             MqttTelegrafPoint(
                 "BRGB",
                 self.device_name,
